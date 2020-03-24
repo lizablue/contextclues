cd4d53f316ccc5d62bcd9e2f875f0c8f9fe1f28e
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-03-24 06:44:49 -0500

    add arrays and attempt h3 accusation listing

--- scripts.js
+++ scripts.js
@@ -1,4 +1,17 @@
-$(document).ready(function() {
 
+// create h3's to display accusations
+function addAcc() {
+    var i = 0;
+    function countAcc() {
+        if (i < 100) {
+            $('div').append('<h3>Accusation ' + i + '</h3>');
+            i++;
+        };
+    }; countAcc();
     
-})
+};
+addAcc();
+
+var friends = ['lily', 'marshall', , 'robin', 'ted', 'barney'];
+var locations = ['house', 'patio', 'bar', 'roof', 'office', 'garage', 'pool', 'cave', 'attic', 'basement'];
+var objects = ['balloon', 'playdoh', 'sharpie', 'robot', 'notebook', 'eraser', 'hat', 'coffee mug', 'lamp', 'earring', 'pillow', 'blanket', 'shoe', 'chair', 'mirror', 'glitter', 'jacket', 'popsicle', 'hammock', 'tissues'];
